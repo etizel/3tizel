51c78a690ad848bdb0dda5b02e18ac407c9ef57b
etiiizel <franciscoetizel@gmail.com>
2023-08-07 13:02:28 -0300

    tailwind

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,7 @@
 export function Hero() {
   return (
-    <section className="bg-gradient-to-tr from-black to-gray-900">
-      <h1 className="text-4xl underline">Hero</h1>
+    <section className="bg-gradient-to-tr from-black to-gray-900 text-white ">
+      <div className="container"></div>
     </section>
   );
 }
