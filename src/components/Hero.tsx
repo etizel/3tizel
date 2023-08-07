00dd41a31133f48e0eb190cc298a9a3ef3dc3c06
etiiizel <franciscoetizel@gmail.com>
2023-08-07 13:36:40 -0300

    tailwind-theme

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,9 @@
 export function Hero() {
   return (
     <section className="bg-gradient-to-tr from-black to-gray-900 text-white ">
-      <div className="container"></div>
+      <div className="container mx-auto m-w-4xl">
+        <h1>Olá, me chamo Francisco Etizel</h1>
+      </div>
     </section>
   );
 }
