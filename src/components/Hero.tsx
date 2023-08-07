494b61d1a661c71b8ee5d613df0963f07eda9f68
etiiizel <franciscoetizel@gmail.com>
2023-08-07 15:43:39 -0300

    Hero - typescript, JSX, React

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,13 @@ export function Hero() {
   return (
     <section className="bg-gradient-to-tr from-black to-gray-900 text-white ">
       <div className="container mx-auto m-w-4xl">
-        <h1>Olá, me chamo Francisco Etizel</h1>
+        <h1>
+          <span className="font-handwriting text-3xl block">Olá, me chamo</span>
+          <span className="font-headline text-5xl font-semibold">
+            Francisco Etizel
+          </span>
+        </h1>
+        <h2>Desenvolvedor React.js FrontEnd</h2>
       </div>
     </section>
   );
