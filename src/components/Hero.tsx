37fc380307a60727f342227382422630ba7a4346
etiiizel <franciscoetizel@gmail.com>
2023-08-07 16:25:21 -0300

    tailwind - JSX

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,17 @@
+import Marksvg from '../img/mark.svg';
 export function Hero() {
   return (
     <section className="bg-gradient-to-tr from-black to-gray-900 text-white ">
       <div className="container mx-auto m-w-4xl">
-        <h1>
+        <h1 className="mb-6">
           <span className="font-handwriting text-3xl block">Olá, me chamo</span>
-          <span className="font-headline text-5xl font-semibold">
-            Francisco Etizel
+          <span className="font-headline text-5xl font-light">Francisco</span>
+          <span className="font-headline text-5xl font-semibold text-pink-900 from-green-900">
+            Etizel
           </span>
         </h1>
-        <h2>Desenvolvedor React.js FrontEnd</h2>
+        <div className="h-1 w-12 rounded-full bg-pink-900"></div>
+        <h2 className="font-semibold">Desenvolvedor React.js FrontEnd</h2>
       </div>
     </section>
   );
