290cf509f0b0b3781177d1b0cb6fed3053651a52
etiiizel <franciscoetizel@gmail.com>
2023-08-15 17:55:30 -0300

    tailwind

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,10 +10,29 @@ export function Hero() {
           </span>
         </h1>
 
-        <h2 className="font-semibold flex items-center">
+        <h2 className="font-semibold flex items-center gap-2">
           <div className="h-1 w-12 rounded-full bg-blue-800" />
           Desenvolvedor React.js Front-End
         </h2>
+
+        <p className="text-gray-600 my-6">
+          Transformando designs em experiências dinâmicas por meio de
+          componentes eficientes e colaborando com a maiores tecnologias do
+          mercado.
+        </p>
+
+        <div className="flex items-center gap-2">
+          <a href="#" className="underline font-bold text-white">
+            Entre em Contato
+          </a>
+          <span className="italic text-gray-500">ou</span>
+          <a
+            href="#"
+            className="cursor-pointer rounded-md bg-orange-600 px-4 py-2 font-headline text-sm font-semibold transition"
+          >
+            Baixe meu CV
+          </a>
+        </div>
       </div>
     </section>
   );
