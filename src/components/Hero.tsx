564fbef25c5dbf1e152a49a20ceda341a5053749
etiiizel <franciscoetizel@gmail.com>
2023-08-28 13:17:03 -0300

    about.tsx section - typescript - JSX - React - Tailwind

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,9 +2,9 @@ import { HiDownload } from 'react-icons/hi';
 export function Hero() {
   return (
     <section className="bg-gradient-to-tr from-black to-gray-900 text-white ">
-      <div className="container mx-auto m-w-4xl p-4 py-4 flex">
+      <div className="container mx-auto m-w-4xl p-4 py-4 flex flex-col md:flex-row">
         <div className="basis-1/2">
-          <h1 className="mb-6">
+          <h1 className="mb-6 text-center md:text-left">
             <span className="font-handwriting text-3xl block my-1 text-center md:text-left">
               Olá, me chamo
             </span>
@@ -14,18 +14,18 @@ export function Hero() {
             </span>
           </h1>
 
-          <h2 className="font-semibold flex items-center gap-2">
+          <h2 className="font-semibold flex items-center gap-2 text-center md:text-left">
             <div className="h-1 w-12 rounded-full bg-blue-800" />
             Desenvolvedor React.js Front-End
           </h2>
 
-          <p className="text-gray-600 my-6">
+          <p className="text-gray-600 my-6 text-center md:text-left">
             Transformando designs em experiências dinâmicas por meio de
             componentes eficientes e colaborando com as mais avançadas
             tecnologias do mercado.
           </p>
 
-          <div className="flex items-center gap-2">
+          <div className="flex items-center justify-center md:justify-start gap-2 ">
             <a href="#" className="underline font-bold text-white">
               Entre em Contato
             </a>
