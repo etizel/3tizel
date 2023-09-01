126389204c289bda52dbc0063a82292392b69f82
etiiizel <franciscoetizel@gmail.com>
2023-09-01 15:34:02 -0300

    deploy

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,34 +3,31 @@ import { HiArrowTopRightOnSquare } from 'react-icons/hi2';
 export default function Projects() {
   const projects = [
     {
-      title: 'Projeto 1',
-      description: 'Descrição do projeto 1',
-      image:
-        'https://images.unsplash.com/photo-1487014679447-9f8336841d58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1605&q=80',
-      link: '#',
+      title: 'Projetos em Algoritmo',
+      description:
+        'Desenvolvimento de um acervo de códigos compilados em javascript/html',
+      image: 'images/project-1.png',
+      link: 'https://github.com/etizel/projects-in-algorithm',
       colSpan: 'col-span-1',
     },
     {
-      title: 'Projeto 2',
-      description: 'Descrição do projeto 2',
-      image:
-        'https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHdlYnNpdGVzfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+      title: 'Animais Fantásticos',
+      description: 'Desenvolvimento de um projeto em javascript, html e css',
+      image: 'images/Animais-Fantásticos.png',
       link: '#',
       colSpan: 'col-span-1 md:col-span-2',
     },
     {
-      title: 'Projeto 3',
-      description: 'Descrição do projeto 3',
-      image:
-        'https://media.istockphoto.com/id/1305995602/photo/responsive-floating-responsive-design.jpg?b=1&s=170667a&w=0&k=20&c=uFqBz27v_B0UxcslnvZxd9c0D9eJAef7veRZf6b-v-A=',
-      link: '#',
+      title: 'Bikecraft',
+      description: 'Desenvolvimento de um projeto em html, css e javascript',
+      image: 'images/bickcraft.png',
+      link: 'https://etizel.github.io/bickcraft/',
       colSpan: 'col-span-1',
     },
     {
-      title: 'Projeto 4',
-      description: 'Descrição do projeto 4',
-      image:
-        'https://images.unsplash.com/photo-1555066931-4365d14bab8c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y29kaW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+      title: 'Lissandra',
+      description: 'Desenvolvido com UI / UX Design, html e css',
+      image: 'images/lissandra.png',
       link: '#',
       colSpan: 'col-span-1',
     },
